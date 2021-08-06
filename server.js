8fac0898889c1c1e68a92ed3c87741c313f1393f
katskurka <kati.skurka@gmail.com>
2021-08-06 19:25:05 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.send('It works!')
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on 1337…') // eslint-disable-line no-console
+})
+
