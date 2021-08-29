d89f01bde92cf2f436611a62d9e7ee168a07f04c
janeishahu <janeishahu@aol.com>
2021-08-29 10:06:07 -0400

    added sequelize and mysql2 depens. Created a models folder and customer.js file

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express')
-
 const app = express()
+const mysql = require('mysql')
+// save for later
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'ZRnQ79X!t_',
+  database: 'Test'
+})
+
 
 app.get('/', (request, response) => {
   return response.send('It works!')
 })
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+  // eslint-disable-next-line no-console
+  console.log('The solution is:', rows[0].solution)
+})
+connection.end()
 
 app.all('*', (request, response) => {
   return response.sendStatus(404)
