c6f7f3e4a6f0cf5a646bf9a54ca7528a1282f58f
janeishahu <janeishahu@aol.com>
2021-09-01 16:48:57 -0400

    removed comments

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ const port = 7000
 
 app.use(express.json())
 
-// Set up the routes to upload the customer data.
-// The datastructure of that data is going to be something like
-// Array of customers' objects
-// [
-//    {
-//      first_name: 'first',
-//      last_name: 'last',
-//      phone_number: '1111111111',
-//      ...
-//    }
-// ]
 customerRoutes(app)
 
 app.listen(port, () => {
