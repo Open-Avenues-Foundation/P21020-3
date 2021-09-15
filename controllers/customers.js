fbac7d2bcd602bd13f04e0e5cab03683ea8c3e71
katskurka <kati.skurka@gmail.com>
2021-09-15 18:26:22 -0400

    typo

--- controllers/customers.js
+++ controllers/customers.js
@@ -24,23 +24,6 @@ const getAllCustomers = async (req, res) => {
 
   return res.send(customers)
 }
-
-const getCustomerById = async (req, res) => {
-  const { Id } = req.params
-
-  const foundCustomer = await models.Customer.findOne({ where: { Id } })
-
-  return res.send(foundCustomer)
-}
-
-const createNewCustomer = async (req, res) => {
-  const { firstName, lastName, email, phoneNumber } = res.body
-
-  // eslint-disable-next-line max-len
-  if (!firstName || !lastName || email || phoneNumber) return res.status(400).send('The following fields are required: firstname, lastname, email, phonenumber')
-
-  const newCustomer = await models.Customer.create({ firstName, lastName, email, phoneNumber })
-
   return res.status(201).send(newCustomer)
 }
 
