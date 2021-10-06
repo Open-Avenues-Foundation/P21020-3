ef05f675c659c3a26cce42352d2e7353ac2fadc7
katskurka <kati.skurka@gmail.com>
2021-10-06 12:20:44 -0400

    added cors, build paths to server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 const express = require('express')
-const customerRoutes = require('./routes/customer')
+const path = require('path')
+const cors = require('cors')
+const customerRoutes = require('./routes/customer.js')
 const groupRoutes = require('./routes/groups')
 const textmessageRoutes = require('./routes/textmessages')
 
 const app = express()
 const port = 7000
 
+app.use(cors())
+app.use(express.static('client/build'))
 
 app.use(express.json())
 
@@ -14,6 +18,7 @@ customerRoutes(app)
 groupRoutes(app)
 textmessageRoutes(app)
 
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client/build', 'index.html')))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
