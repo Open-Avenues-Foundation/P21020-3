501f1e44272f2a2726a5605a02228a5873f49a83
katskurka <kati.skurka@gmail.com>
2021-10-06 11:50:54 -0400

    added initial customer table structure

---
+++ client/src/components/customertable/customerTable.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Id</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>E-mail</th>
+      <th>Phone Number</th>
+      <th>City</th>
+      <th>State</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</Table>
