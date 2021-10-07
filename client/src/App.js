53dea5bcf3b370cd9a03f1ad631acb024213d89e
katskurka <kati.skurka@gmail.com>
2021-10-07 10:40:30 -0400

    got table skeleton working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NewCustomer from './components/InputNewCustomer'
-
+import CustomerTable from './components/CustomerTable/CustomerTable'
 
 
 function App() {
   return (
     <div className="App">
       <NewCustomer />
+      <CustomerTable />
       
     </div>
   );
---
+++ client/src/components/CustomerTable/CustomerTable.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import {Table} from 'react-bootstrap'
+
+const CustomerTable = () => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>E-mail</th>
+          <th>Phone Number</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>motta@gmail.com</td>
+          <td>(508) 476-3211</td>
+          <td>Boston</td>
+          <td>MA</td>
+          <td>edit/delete</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Fox</td>
+          <td>Mulder</td>
+          <td>fmulder@gmail.com</td>
+          <td>(508) 476-3211</td>
+          <td>Boston</td>
+          <td>MA</td>
+          <td>edit/delete</td>
+        </tr>
+      </tbody>
+    </Table>
+      )
+}
+
+export default CustomerTable
