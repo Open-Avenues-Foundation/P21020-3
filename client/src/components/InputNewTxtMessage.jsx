8b4f827576604ffccc63ea1f00e0c532678ae81e
janeishahu <janeishahu@aol.com>
2021-10-16 10:59:51 -0400

    text msg update

--- client/src/components/InputNewTxtMessage.jsx
+++ client/src/components/InputNewTxtMessage.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
 
-const NewMsg = () => {
-        <div className="customerinput">
-            <form action="">
-                <label htmlFor="firstname">To:</label>
-                    <input type="text" name="tofield" id="tofield" required /><br />
-                <label htmlFor="lastname">Message Body:</label>
-                    <input type="text" name="msgbody" id="msgbody" size="60" required />
-                <button className="addcustomer">Create New Message</button>
-            </form>
-        </div>
+
+const NewMsg = (props) => {
+    const {setMessageAdded} = props
+
+    const [newMessage, setNewMessage] = useState({
+        to: '',
+        from: '',
+        body: '',
+    })
+
+const addNewMessage = async (event) => {
+    event.preventDefault()
+    try {
+        const response = await axios.post('http://localhost:7000/newmessage', newMessage)
+       setMessageAdded(true)
+        return response
+    } catch(error) {
+        return error
+    }
 }
 
-export default NewMsg
+
+return (
+        <div className="customerinput">
+            <form action="">
+                <label htmlFor="to">Recipient:</label>
+                    <input type="text" name="to" id="to" value={newMessage.to} onChange={(event) => setNewMessage({to: event.target.value})} required />
+                <label htmlFor="lastName">Last Name:</label>
+                    <input type="text" name="lastName" id="lname" value={newUser.lastName} onChange={(event) => setNewUser({...newUser, lastName: event.target.value})} required />  
+                <label htmlFor="email">Email Address:</label>
+                    <input type="text" name="email" id="email" value={newUser.email} onChange={(event) => setNewUser({...newUser, email: event.target.value})} required /> 
+                <button variant="primary" size="lg" className="addcustomer" onClick={(event) => addNewUser(event)}>Create New Customer</button>
+            </form>
+        </div>
+    )  
+  }
+
+
