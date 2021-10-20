cf8082fb124e0e09fde16c3d50a688377b54f2ce
Brittney Jackson <brnjackson@yahoo.com>
2021-10-20 15:28:20 -0400

    added more styling

--- client/src/components/InputNewCustomer.jsx
+++ client/src/components/InputNewCustomer.jsx
@@ -33,22 +33,22 @@ const addNewUser = async (event) => {
             <h3>Input a New Customer</h3>
             <form action="">
                 <label htmlFor="firstName">First Name:</label>
-                    <input type="text" name="firstName" id="fname" value={newUser.firstName} onChange={(event) => setNewUser({...newUser, firstName: event.target.value})} required />
+                    <input type="text" name="firstName" id="fname" value={newUser.firstName} onChange={(event) => setNewUser({...newUser, firstName: event.target.value})} required /><br />
                 <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" name="lastName" id="lname" value={newUser.lastName} onChange={(event) => setNewUser({...newUser, lastName: event.target.value})} required />  
+                    <input type="text" name="lastName" id="lname" value={newUser.lastName} onChange={(event) => setNewUser({...newUser, lastName: event.target.value})} required /> <br />
                 <label htmlFor="email">Email Address:</label>
-                    <input type="text" name="email" id="email" value={newUser.email} onChange={(event) => setNewUser({...newUser, email: event.target.value})} required />
+                    <input type="text" name="email" id="email" value={newUser.email} onChange={(event) => setNewUser({...newUser, email: event.target.value})} required /><br />
                 <label htmlFor="phoneNumber">Phone Number:</label>
-                    <input type="text" name="phoneNumber" id="phonenumber" value={newUser.phoneNumber} onChange={(event) => setNewUser({...newUser, phoneNumber: event.target.value})} required />
+                    <input type="text" name="phoneNumber" id="phonenumber" value={newUser.phoneNumber} onChange={(event) => setNewUser({...newUser, phoneNumber: event.target.value})} required /><br />
                 <label htmlFor="city">City:</label>
-                    <input type="text" name="city" id="city" value={newUser.city} onChange={(event) => setNewUser({...newUser, city: event.target.value})} required />  
+                    <input type="text" name="city" id="city" value={newUser.city} onChange={(event) => setNewUser({...newUser, city: event.target.value})} required /> <br /> 
                 <label htmlFor="state">State:</label>
-                    <input type="text" name="state" id="state" value={newUser.state} onChange={(event) => setNewUser({...newUser, state: event.target.value})} required />
+                    <input type="text" name="state" id="state" value={newUser.state} onChange={(event) => setNewUser({...newUser, state: event.target.value})} required /><br /> 
                 <label htmlFor="lastOrderDate">Last Order Date:</label>
-                    <input type="text" name="lastOrderDate" id="lastOrderDate" value={newUser.lastOrderDate} onChange={(event) => setNewUser({...newUser, lastOrderDate: event.target.value})} required />
+                    <input type="text" name="lastOrderDate" id="lastOrderDate" value={newUser.lastOrderDate} onChange={(event) => setNewUser({...newUser, lastOrderDate: event.target.value})} required /><br /> 
                 <label htmlFor="lastOrderPrice">Last Order Price:</label>
-                    <input type="text" name="lastOrderPrice" id="lastOrderPrice" value={newUser.lastOrderPrice} onChange={(event) => setNewUser({...newUser, lastOrderPrice: event.target.value})} required />
-                <button variant="primary" size="lg" className="addcustomer" onClick={(event) => addNewUser(event)}>Create New Customer</button>
+                    <input type="text" name="lastOrderPrice" id="lastOrderPrice" value={newUser.lastOrderPrice} onChange={(event) => setNewUser({...newUser, lastOrderPrice: event.target.value})} required /><br /> 
+                <button variant="primary" size="lg" className="addcustomer" onClick={(event) => addNewUser(event)}>Create New Customer</button> <input type="reset" value="reset" />
             </form>
         </div>
     )  
